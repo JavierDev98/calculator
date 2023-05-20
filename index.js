f22faf83591b17b8e1c3882a9aa75881236e7bf2
JavierDev98 <javiersniper1@gmail.com>
2023-05-19 22:26:43 -0600

    simplified code in the buttons for the numbers

--- index.js
+++ index.js
@@ -74,6 +74,16 @@ function operate(a,b,operator) {
 }
 
 //Functionality for display and all of the buttons.
+
+function displayNumbers() {
+  if(operator == null) {
+    display.innerText = a.join('');
+  } 
+  else {
+    display.innerText = b.join('');
+}
+}
+
 let displayText = document.createElement("p");
 displayText.classList.add("displayText");
 displayText.innerText = "Created by JaviDev98";
@@ -83,61 +93,50 @@ display.innerHTML = displayText.innerText;
 
 let buttonZero = document.querySelector("#zero");
 buttonZero.addEventListener('click', function () {operator == null ? a.push(0) : b.push(0)});
-buttonZero.addEventListener('click',function() {; 
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonZero.addEventListener('click', displayNumbers);
 
 let buttonOne = document.querySelector("#one");
 buttonOne.addEventListener('click', function () {operator == null ? a.push(1) : b.push(1)});
-buttonOne.addEventListener('click',function() {
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonOne.addEventListener('click', displayNumbers);
 
 
 let buttonTwo = document.querySelector("#two");
 buttonTwo.addEventListener('click', function () {operator == null ? a.push(2) : b.push(2)});
-buttonTwo.addEventListener('click',function() { 
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonTwo.addEventListener('click', displayNumbers);
 
 
 let buttonThree = document.querySelector("#three");
 buttonThree.addEventListener('click', function () {operator == null ? a.push(3) : b.push(3)});
-buttonThree.addEventListener('click',function() {
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonThree.addEventListener('click', displayNumbers);
 
 
 let buttonFour = document.querySelector("#four");
 buttonFour.addEventListener('click', function () {operator == null ? a.push(4) : b.push(4)});
-buttonFour.addEventListener('click',function() {
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
-
+buttonFour.addEventListener('click', displayNumbers);
 
 let buttonFive = document.querySelector("#five");
 buttonFive.addEventListener('click', function () {operator == null ? a.push(5) : b.push(5)});
-buttonFive.addEventListener('click',function() {
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonFive.addEventListener('click', displayNumbers);
 
 
 let buttonSix = document.querySelector("#six");
 buttonSix.addEventListener('click', function () {operator == null ? a.push(6) : b.push(6)});
-buttonSix.addEventListener('click',function() { 
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonSix.addEventListener('click', displayNumbers);
 
 
 let buttonSeven = document.querySelector("#seven");
 buttonSeven.addEventListener('click', function () {operator == null ? a.push(7) : b.push(7)});
-buttonSeven.addEventListener('click',function() { 
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonSeven.addEventListener('click', displayNumbers);
 
 
 let buttonEight = document.querySelector("#eight");
 buttonEight.addEventListener('click', function () {operator == null ? a.push(8) : b.push(8)});
-buttonEight.addEventListener('click',function() {
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonEight.addEventListener('click', displayNumbers);
 
 
 let buttonNine = document.querySelector("#nine");
 buttonNine.addEventListener('click', function () {operator == null ? a.push(9) : b.push(9)});
-buttonNine.addEventListener('click',function() {   
-if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonNine.addEventListener('click', displayNumbers);
 
 
 let clear = document.querySelector("#clear");
