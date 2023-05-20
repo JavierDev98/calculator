73bae65dc2e4e2694c7d10e7d906a79006a7f3f8
JavierDev98 <javiersniper1@gmail.com>
2023-05-19 22:21:14 -0600

    fixed bug that would make the calculator only operate the first digit!

--- index.js
+++ index.js
@@ -27,48 +27,49 @@ let b = [];
 let operator;
 let index = 0; //index indicates if the function has already been used.
 function operate(a,b,operator) {
-  debugger;
-  a = parseInt(a.join(''));
-  b = parseInt(b.join(''));
+  let aOp = parseInt(a.join(''));
+  let bOp = parseInt(b.join(''));
+  b.length = 0;
     if(index < 1) {
         if(operator === "+") {
-            result = add(a,b);
-            index++;
+            result = add(aOp,bOp);
+            
         }
         else if(operator === "-") {
-          result = subtract(a,b);
-          index++;
+          result = subtract(aOp,bOp);
+          
         }
         else if(operator === "*") {
-          result = multiply(a,b);
-          index++;
+          result = multiply(aOp,bOp);
+        
         }
         else if(operator === "/") {
-          result = divide(a,b);
-          index++;
+          result = divide(aOp,bOp);
+        
         }
         else if(operator === "%") {
-          result = CalcModulo(a,b);
-          index++;
+          result = CalcModulo(aOp,bOp);
         }
     }
    else {
     if(operator === "+") {
-        result = add(result,b);
+        result = add(result,bOp);
     }
     else if(operator === "-") {
-      result = subtract(result,b);
+      result = subtract(result,bOp);
     }
     else if(operator === "*") {
-      result = multiply(result,b);
+      result = multiply(result,bOp);
     }
     else if(operator === "/") {
-      result = divide(result,b);
+      result = divide(result,bOp);
     }
     else if(operator === "%") {
-      result = modulo(result,b);
+      result = modulo(result,bOp);
     }
    }
+   index++;
+ 
   return result;
 }
 
@@ -80,66 +81,71 @@ displayText.innerText = "Created by JaviDev98";
 let display = document.querySelector("#display");
 display.innerHTML = displayText.innerText;
 
+let buttonZero = document.querySelector("#zero");
+buttonZero.addEventListener('click', function () {operator == null ? a.push(0) : b.push(0)});
+buttonZero.addEventListener('click',function() {; 
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+
 let buttonOne = document.querySelector("#one");
 buttonOne.addEventListener('click', function () {operator == null ? a.push(1) : b.push(1)});
-buttonOne.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonOne.addEventListener('click',function() {
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let buttonTwo = document.querySelector("#two");
 buttonTwo.addEventListener('click', function () {operator == null ? a.push(2) : b.push(2)});
-buttonTwo.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonTwo.addEventListener('click',function() { 
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let buttonThree = document.querySelector("#three");
 buttonThree.addEventListener('click', function () {operator == null ? a.push(3) : b.push(3)});
-buttonThree.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonThree.addEventListener('click',function() {
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let buttonFour = document.querySelector("#four");
 buttonFour.addEventListener('click', function () {operator == null ? a.push(4) : b.push(4)});
-buttonFour.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonFour.addEventListener('click',function() {
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let buttonFive = document.querySelector("#five");
 buttonFive.addEventListener('click', function () {operator == null ? a.push(5) : b.push(5)});
-buttonFive.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonFive.addEventListener('click',function() {
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let buttonSix = document.querySelector("#six");
 buttonSix.addEventListener('click', function () {operator == null ? a.push(6) : b.push(6)});
-buttonSix.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonSix.addEventListener('click',function() { 
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let buttonSeven = document.querySelector("#seven");
 buttonSeven.addEventListener('click', function () {operator == null ? a.push(7) : b.push(7)});
-buttonSeven.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonSeven.addEventListener('click',function() { 
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let buttonEight = document.querySelector("#eight");
 buttonEight.addEventListener('click', function () {operator == null ? a.push(8) : b.push(8)});
-buttonEight.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonEight.addEventListener('click',function() {
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let buttonNine = document.querySelector("#nine");
 buttonNine.addEventListener('click', function () {operator == null ? a.push(9) : b.push(9)});
-buttonNine.addEventListener('click',function() {a.join(','); b.join(''); 
-if(index < 1) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
+buttonNine.addEventListener('click',function() {   
+if(operator == null) {display.innerText = a.join('');} else {display.innerText = b.join('')}});
 
 
 let clear = document.querySelector("#clear");
 clear.addEventListener('click',function () {a = []; b = []; operator = null; result = 0; index = 0; display.innerText = "Created by JaviDev98";})
 
 let equals = document.querySelector("#equals");
-equals.addEventListener('click', operate(a,b,operator));
-equals.addEventListener('click', function()  {display.innerText = operate(a,b,operator)});
+equals.addEventListener('click', function() { index < 1 ? operate(a,b,operator) : operate(result,b,operator)});
+equals.addEventListener('click', function(){display.innerText = result;});
 
 let sum = document.querySelector("#add");
 sum.addEventListener('click',function() {operator = "+"});
